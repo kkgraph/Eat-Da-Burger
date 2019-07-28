13d2d5ced1d0d132dde88a17b43924076d8dd752
Kim Graff <kimagraff@gmail.com>
2019-07-28 13:50:21 -0400

    updating database

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,3 @@
--- create DATABASE burgers_db;
--- For development
--- USE burgers_db;
--- From https://fresca-burgers.herokuapp.com/
--- USE un33jcqv3cnjgsif;
-
 CREATE TABLE burgers
 (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,6 @@
 INSERT INTO burgers(burger_name) VALUES
-('Fresh Burger'),
-('Atomic Burger'),
-('New Mexico Burger'),
-('Philly Cheese Burger'),
-('Mon Ami Burger'),
-('Gunslinger Burger'),
-('The Dude Burger');
+('Gauc Burger'),
+('Tofu Burger'),
+('Black Bean Burger'),
+('Vegan Cheese Burger'),
+('Veggie Burger'),
