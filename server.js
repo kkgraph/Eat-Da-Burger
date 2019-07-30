a3e727fbdeee06510474d230eec33c926457505b
Kim Graff <kimagraff@gmail.com>
2019-07-30 18:54:58 -0400

    cleaning server code

--- server.js
+++ server.js
@@ -2,22 +2,22 @@ var express = require("express");
 var PORT = process.env.PORT || 8080;
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
+//Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// parse application/x-www-form-urlencoded
+//Application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-// parse application/json
+//Application/json
 app.use(express.json());
 
-// Set Handlebars.
+//Set Handlebars.
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
+//Import routes and give the server access to them.
 var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
